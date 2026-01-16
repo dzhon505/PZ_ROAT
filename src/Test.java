8146b1b09509a5d61c72923d952bb87705bd630d
rogovoy <evgenroo@gmail.com>
2026-01-16 09:32:47 +0300

    test2 at work

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Test {
     static void main(String[] args) {
-        System.out.println("hello");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя файла: ");
+        String fileName = scanner.nextLine();
+        int dotCount = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            int ch;
+            while ((ch = reader.read()) != -1){
+                if (ch == '.'){
+                    dotCount++;
+                }
+            }System.out.println(dotCount);
+        }catch (IOException e){
+            System.out.println("Error! " + e.getMessage());
+        }
     }
 }
